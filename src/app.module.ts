ef8ae23dc0aa2b44e2c5cd48ec692f2f888813af
송유현 <68471917+songyouhyun@users.noreply.github.com>
2023-03-14 16:20:56 +0900

    feat: 환경변수 설정 및 유효성 검사

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './config/env.validation';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, validate: validate })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: false });
+  const port: number = app.get(ConfigService).get<number>('PORT');
+  await app.listen(port, '0.0.0.0');
+  Logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
