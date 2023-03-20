5ee455d78924f0e411ea2fbaa3f512ead9447fe8
송유현 <68471917+songyouhyun@users.noreply.github.com>
2023-03-20 15:15:52 +0900

    Revert "feat: 환경변수 설정 및 유효성 검사"
    This reverts commit ef8ae23dc0aa2b44e2c5cd48ec692f2f888813af.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { validate } from './config/env.validation';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, validate: validate })],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
