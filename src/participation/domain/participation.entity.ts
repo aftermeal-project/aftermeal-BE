4589f7b85b8b9c84a057a6eb07c0d87b85b830eb
송유현 <68471917+songyouhyun@users.noreply.github.com>
2023-10-24 14:05:05 +0900

    selectedActivity 삭제

--- src/participation/domain/participation.entity.ts
+++ src/participation/domain/participation.entity.ts
@@ -2,18 +2,20 @@ import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 import { BaseTimeEntity } from '../../common/model/base-time.entity';
 import { User } from '../../user/domain/user.entity';
 import { Activity } from '../../activity/domain/activity.entity';
-import { SelectedActivity } from '../../activity/domain/selected-activity.entity';
 
 @Entity()
 export class Participation extends BaseTimeEntity {
   @PrimaryColumn()
   userId: number;
 
+  @PrimaryColumn()
+  activityId: number;
+
   @ManyToOne(() => User)
   @JoinColumn({ name: 'user_id' })
   user: User;
 
-  @ManyToOne(() => Activity, { cascade: true })
-  @JoinColumn({ name: 'selected_activity_id' })
-  selectedActivity: SelectedActivity;
+  @ManyToOne(() => Activity)
+  @JoinColumn({ name: 'activity_id' })
+  activity: Activity;
 }
