1c7238a96e745c6b0e8f99da28988b4d8ad9ef20
송유현 <68471917+songyouhyun@users.noreply.github.com>
2023-11-30 14:16:29 +0900

    글로벌 미들웨어 적용

---
+++ src/common/utils/set-nest-app.ts
@@ -0,0 +1,20 @@
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+/**
+ * 글로벌 미들웨어 구성을 모아두는 함수입니다.
+ * 모노레포 환경에서 여러 모듈에서 공통적으로 사용하고,
+ * E2E (End To End) 테스트에서도 실 서비스와 동일한 인터셉터 설정을 위해 공통적으로 사용하기 위함
+ * @param {INestApplication} app
+ */
+export function setNestApp<T extends INestApplication>(app: T): void {
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(
+    new ValidationPipe({ transform: true, validateCustomDecorators: true }),
+  );
+  app.setGlobalPrefix('api');
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigType } from '@nestjs/config';
-import appConfig from '../src/config/app.config';
+import appConfig from '@config/app.config';
 import { Logger } from '@nestjs/common';
+import { setNestApp } from '@common/utils/set-nest-app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
   const port: number = config.port;
   const host: string = config.host;
-  await app.listen(port, host, async () => Logger.log(`Application is running on: ${await app.getUrl()}`));
+  setNestApp(app);
+  await app.listen(port, host, async () =>
+    Logger.log(`Application is running on: ${await app.getUrl()}`),
+  );
 }
 void bootstrap();
