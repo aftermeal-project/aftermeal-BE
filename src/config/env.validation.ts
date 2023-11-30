822ea26b2138cdedf857d37409fc486b98c496d3
송유현 <68471917+songyouhyun@users.noreply.github.com>
2023-11-30 14:01:56 +0900

    database type을 환경변수로 관리 X

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -18,9 +18,6 @@ class EnvironmentVariables {
   PORT: number;
 
   @IsString()
-  DB_TYPE: string;
-
-  @IsString()
   DB_HOST: string;
 
   @IsNumber()
@@ -34,17 +31,39 @@ class EnvironmentVariables {
 
   @IsString()
   DB_NAME: string;
+
+  @IsString()
+  ACCESS_TOKEN_SECRET: string;
+
+  @IsString()
+  REFRESH_TOKEN_SECRET: string;
+
+  @IsNumber()
+  ACCESS_TOKEN_EXPIRY_TIME: number;
+
+  @IsNumber()
+  REFRESH_TOKEN_EXPIRY_TIME: number;
+
+  @IsNumber()
+  CACHE_INVITATION_TTL: number;
 }
 
-export const validate = (config: Record<string, unknown>): EnvironmentVariables => {
-  const validateConfig: EnvironmentVariables = plainToClass(EnvironmentVariables, config, {
-    enableImplicitConversion: true,
-  });
+export const validate = (
+  config: Record<string, unknown>,
+): EnvironmentVariables => {
+  const validateConfig: EnvironmentVariables = plainToClass(
+    EnvironmentVariables,
+    config,
+    {
+      enableImplicitConversion: true,
+    },
+  );
   const errors: ValidationError[] = validateSync(validateConfig, {
     skipMissingProperties: false,
   });
 
-  if (errors.length > 0) throw new Error(errors.toString());
-
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
   return validateConfig;
 };
