bf662eaa7e8eb411a5813044b7c6930f02f03606
송유현 <68471917+songyouhyun@users.noreply.github.com>
2023-11-30 13:58:11 +0900

    database type을 환경변수로 관리 X

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  type: process.env.DB_TYPE as any,
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10) || 3306,
   user: process.env.DB_USER,
