00fdcd0f743a4bae45be02c8a19a533f9733f457
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-01-04 12:30:14 +0900

    chore: invitation Controller 경로 변경

--- src/modules/invitation/presentation/invitation.controller.ts
+++ src/modules/invitation/presentation/invitation.controller.ts
@@ -5,14 +5,14 @@ import { ResponseEntity } from '@common/model/response.entity';
 import { InvitationMemberService } from '../application/invitation-member.service';
 import { MemberTypeValidationPipe } from '@common/pipe/member-type-validation.pipe';
 
-@Controller('v1')
+@Controller('v1/invitation')
 export class InvitationController {
   constructor(
     @Inject(InvitationMemberService)
     private readonly invitationService: InvitationService,
   ) {}
 
-  @Post('invitation/member')
+  @Post('member')
   async invite(
     @Body(MemberTypeValidationPipe) invitationForm: InvitationForm,
   ): Promise<ResponseEntity<[]>> {
