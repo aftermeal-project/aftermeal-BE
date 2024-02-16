26e6db5c6b5488e08b6ce8a5c0fc5ffdd76e7f3a
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-02-16 17:48:26 +0900

    환경 변수 유효성 검사 규칙 추가(IsNotEmpty)

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -1,5 +1,10 @@
 import { plainToClass } from 'class-transformer';
-import { IsEnum, validateSync, ValidationError } from 'class-validator';
+import {
+  IsEnum,
+  IsNotEmpty,
+  validateSync,
+  ValidationError,
+} from 'class-validator';
 import { IsNumber, IsString } from 'class-validator';
 
 enum NodeEnvironment {
@@ -9,63 +14,83 @@ enum NodeEnvironment {
 
 class EnvironmentVariables {
   @IsEnum(NodeEnvironment)
+  @IsNotEmpty()
   NODE_ENV: NodeEnvironment;
 
   @IsString()
+  @IsNotEmpty()
   BASE_URL: string;
 
   @IsString()
+  @IsNotEmpty()
   HOST: string;
 
   @IsNumber()
+  @IsNotEmpty()
   PORT: number;
 
   @IsString()
+  @IsNotEmpty()
   DB_HOST: string;
 
   @IsNumber()
+  @IsNotEmpty()
   DB_PORT: number;
 
   @IsString()
+  @IsNotEmpty()
   DB_USER: string;
 
   @IsString()
+  @IsNotEmpty()
   DB_PW: string;
 
   @IsString()
+  @IsNotEmpty()
   DB_NAME: string;
 
   @IsString()
+  @IsNotEmpty()
   ACCESS_TOKEN_SECRET: string;
 
   @IsNumber()
+  @IsNotEmpty()
   ACCESS_TOKEN_EXPIRY_TIME: number;
 
   @IsString()
+  @IsNotEmpty()
   REFRESH_TOKEN_SECRET: string;
 
   @IsNumber()
+  @IsNotEmpty()
   REFRESH_TOKEN_EXPIRY_TIME: number;
 
   @IsString()
+  @IsNotEmpty()
   JWT_ISSUER: string;
 
   @IsNumber()
+  @IsNotEmpty()
   INVITATION_EXPIRY_TIME: number;
 
   @IsString()
+  @IsNotEmpty()
   CACHE_HOST: string;
 
   @IsNumber()
+  @IsNotEmpty()
   CACHE_PORT: number;
 
   @IsString()
+  @IsNotEmpty()
   EMAIL_SERVICE: string;
 
   @IsString()
+  @IsNotEmpty()
   EMAIL_AUTH_USER: string;
 
   @IsString()
+  @IsNotEmpty()
   EMAIL_AUTH_PASSWORD: string;
 }
 
