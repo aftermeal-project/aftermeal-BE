fc9c46d259c32bba94c7b45907ff5dd0073d7b54
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-03-04 16:45:15 +0900

    delete: global prefix 'api'

--- src/common/utils/src/set-nest-app.ts
+++ src/common/utils/src/set-nest-app.ts
@@ -9,6 +9,5 @@ import { Reflector } from '@nestjs/core';
  */
 export function setNestApp<T extends INestApplication>(app: T): void {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  app.setGlobalPrefix('api');
   app.enableCors();
 }
