8965fd639806b8320f6325100b665504ebaf442a
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-04-23 22:38:26 +0900

    chore: UserRoleRepository 의존성 추가

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -5,9 +5,14 @@ import { RoleModule } from '../role/role.module';
 import { GenerationModule } from '../generation/generation.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './domain/user.entity';
+import { UserRole } from './domain/user-role.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), RoleModule, GenerationModule],
+  imports: [
+    TypeOrmModule.forFeature([User, UserRole]),
+    RoleModule,
+    GenerationModule,
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
