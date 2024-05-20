778a69993d236c887f87a1eed6f8ad55030d58f6
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-05-20 23:38:20 +0900

    chore: 참가 성공 응답 메시지 수정

--- src/modules/participation/presentation/participation.controller.ts
+++ src/modules/participation/presentation/participation.controller.ts
@@ -14,6 +14,6 @@ export class ParticipationController {
     @User('userId') userId: number,
   ): Promise<ResponseEntity<void>> {
     await this.participationService.apply(dto.activityId, userId);
-    return ResponseEntity.OK_WITH('참가 신청 성공');
+    return ResponseEntity.OK_WITH('참가 신청에 성공했습니다.');
   }
 }
