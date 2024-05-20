e3cb2c838c4b83e45792edcd9d0e2a61c00d421d
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-05-20 23:19:20 +0900

    test: ParticipationService 통합테스트 작성

--- src/modules/participation/participation.module.ts
+++ src/modules/participation/participation.module.ts
@@ -4,9 +4,14 @@ import { ParticipationService } from './application/participation.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Participation } from './domain/participation.entity';
 import { ActivityModule } from '../activity/activity.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Participation]), ActivityModule],
+  imports: [
+    TypeOrmModule.forFeature([Participation]),
+    ActivityModule,
+    UserModule,
+  ],
   controllers: [ParticipationController],
   providers: [ParticipationService],
 })
