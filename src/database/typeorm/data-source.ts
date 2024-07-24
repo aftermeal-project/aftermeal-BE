531b69ba71a448431c58477c907d1bdf573cda7d
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-07-24 16:32:22 +0900

    feat: typeorm migration 스크립트 추가 - typeorm cli를 위한 별도의 data-source 파일 생성

---
+++ src/database/typeorm/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [join(__dirname, '../../modules/**/domain/*.entity{.ts,.js}')],
+  migrations: [join(__dirname, '../../database/migrations/*{.ts,.js}')],
+});
+
+export const getDataSource = () => AppDataSource;
