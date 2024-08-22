3a1d7053146208891762d4795c9918c324058703
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-08-22 22:39:22 +0900

    refactor: 마이그레이션 파일에서 불필요한 쿼리문 개선

--- src/database/migrations/1724068871436-Migration.ts
+++ src/database/migrations/1724068871436-Migration.ts
@@ -7,17 +7,9 @@ export class Migration1724068871436 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE \`user\` CHANGE \`user_type\` \`type\` varchar(255) NOT NULL`,
     );
-    await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`type\``);
-    await queryRunner.query(
-      `ALTER TABLE \`user\` ADD \`type\` varchar(255) NOT NULL`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`type\``);
-    await queryRunner.query(
-      `ALTER TABLE \`user\` ADD \`type\` varchar(255) NOT NULL`,
-    );
     await queryRunner.query(
       `ALTER TABLE \`user\` CHANGE \`type\` \`user_type\` varchar(255) NOT NULL`,
     );
