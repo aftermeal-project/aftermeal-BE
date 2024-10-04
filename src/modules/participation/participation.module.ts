db3959e8ed62e453f8dda2d9332dc6a3cb7c381f
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-10-04 14:41:33 +0900

    fix: ParticipationService 의존성 변경

--- src/modules/participation/participation.module.ts
+++ src/modules/participation/participation.module.ts
@@ -2,24 +2,33 @@ import { Module } from '@nestjs/common';
 import { ParticipationService } from './application/services/participation.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Participation } from './domain/entities/participation.entity';
-import { PARTICIPATION_REPOSITORY } from '@common/constants/dependency-token';
+import {
+  ACTIVITY_REPOSITORY,
+  PARTICIPATION_REPOSITORY,
+  TIME,
+} from '@common/constants/dependency-token';
 import { ParticipationTypeormRepository } from './infrastructure/persistence/participation-typeorm.repository';
-import { ActivityModule } from '../activity/activity.module';
-import { UserModule } from '../user/user.module';
 import { ParticipationController } from './presentation/controllers/participation.controller';
+import { JodaTime } from '@common/time/joda-time';
+import { ActivityTypeormRepository } from '../activity/infrastructure/persistence/activity-typeorm.repository';
+import { Activity } from '../activity/domain/entities/activity.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Participation]),
-    ActivityModule,
-    UserModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Participation, Activity])],
   controllers: [ParticipationController],
   providers: [
     ParticipationService,
     {
       provide: PARTICIPATION_REPOSITORY,
       useClass: ParticipationTypeormRepository,
+    },
+    {
+      provide: ACTIVITY_REPOSITORY,
+      useClass: ActivityTypeormRepository,
+    },
+    {
+      provide: TIME,
+      useClass: JodaTime,
     },
   ],
 })
