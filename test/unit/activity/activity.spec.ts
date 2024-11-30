137d992bdcb2d8ddf11e12c18f7dc9f1f3254238
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-11-30 16:34:11 +0900

    test: Activity 도메인 로직 단위 테스트 작성

--- test/unit/activity/activity.spec.ts
+++ test/unit/activity/activity.spec.ts
@@ -4,6 +4,8 @@ import { ActivityLocation } from '../../../src/modules/activity-location/domain/
 import { EActivityType } from '../../../src/modules/activity/domain/entities/activity-type';
 import { IllegalStateException } from '@common/exceptions/illegal-state.exception';
 import { IllegalArgumentException } from '@common/exceptions/illegal-argument.exception';
+import { User } from '../../../src/modules/user/domain/entities/user.entity';
+import { Participation } from '../../../src/modules/participation/domain/entities/participation.entity';
 
 describe('Activity', () => {
   describe('create', () => {
@@ -52,6 +54,114 @@ describe('Activity', () => {
 
       // then
       expect(result).toThrowError(IllegalStateException);
+    });
+  });
+
+  describe('isFull', () => {
+    it('참가 인원이 꽉 찼을 때 true를 반환한다.', () => {
+      // given
+      const now: ZonedDateTime = ZonedDateTime.of(
+        LocalDate.of(2024, 10, 30),
+        LocalTime.of(8, 30, 0),
+        ZoneOffset.UTC,
+      );
+      const activity: Activity = Activity.create(
+        'title',
+        1,
+        new ActivityLocation(),
+        EActivityType.LUNCH,
+        LocalDate.of(2024, 10, 30),
+        now,
+      );
+
+      activity.participations = [new Participation()];
+
+      // when
+      const result = activity.isFull();
+
+      // then
+      expect(result).toBe(true);
+    });
+
+    it('참가 인원이 꽉 차지 않았을 때 false를 반환한다.', () => {
+      // given
+      const now: ZonedDateTime = ZonedDateTime.of(
+        LocalDate.of(2024, 10, 30),
+        LocalTime.of(8, 30, 0),
+        ZoneOffset.UTC,
+      );
+      const activity: Activity = Activity.create(
+        'title',
+        2,
+        new ActivityLocation(),
+        EActivityType.LUNCH,
+        LocalDate.of(2024, 10, 30),
+        now,
+      );
+
+      activity.participations = [new Participation()];
+
+      // when
+      const result = activity.isFull();
+
+      // then
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('hasParticipation', () => {
+    it('해당 참가자가 참가한 사용자면 true를 반환한다.', () => {
+      // given
+      const now: ZonedDateTime = ZonedDateTime.of(
+        LocalDate.of(2024, 10, 30),
+        LocalTime.of(8, 30, 0),
+        ZoneOffset.UTC,
+      );
+      const activity: Activity = Activity.create(
+        'title',
+        2,
+        new ActivityLocation(),
+        EActivityType.LUNCH,
+        LocalDate.of(2024, 10, 30),
+        now,
+      );
+
+      const user: User = new User();
+      user.id = 1;
+
+      const participation: Participation = new Participation();
+      participation.user = user;
+
+      activity.participations = [participation];
+
+      // when
+      const result = activity.hasParticipation(user);
+
+      // then
+      expect(result).toBe(true);
+    });
+
+    it('해당 참가자가 참가한 사용자가 아니면 false를 반환한다.', () => {
+      // given
+      const now = ZonedDateTime.of(
+        LocalDate.of(2024, 10, 30),
+        LocalTime.of(8, 30, 0),
+        ZoneOffset.UTC,
+      );
+      const activity: Activity = Activity.create(
+        'title',
+        2,
+        new ActivityLocation(),
+        EActivityType.LUNCH,
+        LocalDate.of(2024, 10, 30),
+        now,
+      );
+
+      // when
+      const result = activity.hasParticipation(new User());
+
+      // then
+      expect(result).toBe(false);
     });
   });
 });
