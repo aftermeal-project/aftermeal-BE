d1987815bb6432b5dba75b822ec7f2ceab6e3320
송유현 <68471917+songyouhyun@users.noreply.github.com>
2024-11-30 13:41:43 +0900

    test: 활동 생성 로직 변경에 대한 테스트 작성

--- test/unit/activity/activity.spec.ts
+++ test/unit/activity/activity.spec.ts
@@ -3,45 +3,13 @@ import { LocalDate, LocalTime, ZonedDateTime, ZoneOffset } from '@js-joda/core';
 import { ActivityLocation } from '../../../src/modules/activity-location/domain/entities/activity-location.entity';
 import { EActivityType } from '../../../src/modules/activity/domain/entities/activity-type';
 import { IllegalStateException } from '@common/exceptions/illegal-state.exception';
+import { IllegalArgumentException } from '@common/exceptions/illegal-argument.exception';
 
 describe('Activity', () => {
   describe('create', () => {
-    it('활동을 생성한다.', () => {
+    it('활동 예정 날짜는 과거로 설정할 수 없다.', () => {
       // given
-      const activityLocation: ActivityLocation = new ActivityLocation();
-
-      const now: ZonedDateTime = ZonedDateTime.of(
-        LocalDate.of(2024, 1, 1),
-        LocalTime.of(0, 0),
-        ZoneOffset.UTC,
-      );
-
-      const scheduledDate: LocalDate = LocalDate.of(2024, 1, 1);
-
-      // when
-      const activity: Activity = Activity.create(
-        'title',
-        10,
-        activityLocation,
-        EActivityType.LUNCH,
-        scheduledDate,
-        now,
-      );
-
-      // then
-      expect(activity.title).toEqual('title');
-      expect(activity.maxParticipants).toEqual(10);
-      expect(activity.location).toEqual(activityLocation);
-      expect(activity.type).toEqual(EActivityType.LUNCH);
-      expect(activity.scheduledDate).toEqual(scheduledDate);
-      expect(activity.startAt).toEqual('');
-    });
-
-    it('과거 날짜로 생성할 수 없다.', () => {
-      // given
-      const activityLocation = new ActivityLocation();
-
-      const pastDay: LocalDate = LocalDate.of(2024, 10, 1);
+      const scheduledDate: LocalDate = LocalDate.of(2024, 10, 29);
       const now: ZonedDateTime = ZonedDateTime.of(
         LocalDate.of(2024, 10, 30),
         LocalTime.of(0, 0),
@@ -53,24 +21,22 @@ describe('Activity', () => {
         Activity.create(
           'title',
           10,
-          activityLocation,
+          new ActivityLocation(),
           EActivityType.LUNCH,
-          pastDay,
+          scheduledDate,
           now,
         );
 
       // then
-      expect(result).toThrowError(IllegalStateException);
+      expect(result).toThrowError(IllegalArgumentException);
     });
 
-    it('당일 활동은 활동 시작 4시간 전까지만 생성할 수 있다.', () => {
+    it('활동 시작 시간 이후엔 활동을 생성할 수 없다.', () => {
       // given
-      const activityLocation = new ActivityLocation();
-
-      const today: LocalDate = LocalDate.of(2024, 10, 30);
+      const scheduledDate: LocalDate = LocalDate.of(2024, 10, 30);
       const now: ZonedDateTime = ZonedDateTime.of(
-        today,
-        LocalTime.of(11, 30),
+        LocalDate.of(2024, 10, 30),
+        LocalTime.of(12, 31),
         ZoneOffset.UTC,
       );
 
@@ -79,9 +45,9 @@ describe('Activity', () => {
         Activity.create(
           'title',
           10,
-          activityLocation,
+          new ActivityLocation(),
           EActivityType.LUNCH,
-          today,
+          scheduledDate,
           now,
         );
 
